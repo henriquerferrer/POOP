25164829803cbb27a1b41bbfd0c78e7b551e5333
Joaquim Vaz Ramires Borges Ferrer <jferrer@student.dei.uc.pt>
2017-12-11 01:41:03 +0000

    Adds gestorBaseDados class barebone

---
+++ GestorBaseDados.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class GestorBaseDados {
+	private final String pessoasPath = "./bd/pessoas/";
+	private final String locaisPath = "./bd/locais/";
+	private final String inscricoesPath = "./bd/inscricoes/";
+	
+	public GestorBaseDados() {}
+	
+	/*
+	public ArrayList<Pessoa> loadPessoas() {
+		
+	}
+	*/
+}
